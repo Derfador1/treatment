c0bb52df0c4172db590633bc87a65e5dbbc6163d
Derfador1 <Derfador1@gmail.com>
2016-07-19 14:19:24 -0400

    adding my waste server

---
+++ waste_server.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import os
+import socket
+import sys
+import struct
+import queue
+import scrypt
+import threading
+
+q = queue.Queue()
+
+class Header:
+	def __init__(self, type1, size, custom):
+		self.type1 = type1
+		self.size = size
+		self.custom = custom
+
+	def serialize(self):
+		return struct.pack("!HHL", self.type1, self.size, self.custom)
+
+def worker():
+	while True:
+		item = q.get()
+		if item is None:
+		    break
+		waste(item)
+		q.task_done()
+
+#Found scrypt methods at https://pypi.python.org/pypi/scrypt/
+def waste(data):
+	print(data)
+	if data:
+		outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		outgoing.connect(("localhost", 2345))
+		listo = []
+		listo = data.split(',')
+		header = Header(4, 8+len(listo)*8, 0)
+		outgoing.send(header.serialize())
+		for i in listo:
+			i = int(i)
+			h1 = struct.pack("!LL", i, 0)
+			outgoing.send(h1)
+		outgoing.close()
+
+def main():
+	num_worker_threads = 2
+
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	server.bind(('127.0.0.1', 40003))	
+
+	server.listen(num_worker_threads)
+
+	threads = []
+	for i in range(num_worker_threads):
+		t = threading.Thread(target=worker)
+		t.start()
+		threads.append(t)
+
+	recieved = []	
+
+	while server:
+		try:
+			if len(threads) < num_worker_threads:
+				t = threading.Thread(target=worker)
+				t.start()
+				threads.append(t)
+			try:
+				conn, addr = server.accept()
+			except Exception:
+				break
+			if conn:
+				try:
+					conn.settimeout(5)
+					val = conn.recv(1024).decode('utf-8')
+					recieved.append(val)
+				except socket.timeout:
+					pass
+
+			for item in recieved:
+				q.put(item)
+				recieved.remove(item)	
+				
+		except KeyboardInterrupt:
+			print("Caught")
+
+			# block until all tasks are done
+			q.join()
+
+			# stop workers
+			for i in range(num_worker_threads):
+				q.put(None)
+			for t in threads:
+				t.join()
+				
+			server.close()
+
+if __name__ == "__main__":
+	main()
